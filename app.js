56d1200631c05a238dee7a55be5d359689501499
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-03-01 11:16:42 +0100

    added video

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/FrontPage/frontpage.html");
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 });
 
 
